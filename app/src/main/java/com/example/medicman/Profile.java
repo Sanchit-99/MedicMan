f0f09e2d342422572588c9a217c634495b3b47e3
Sanchit-99 <sanchitgupta187@gmail.com>
2020-07-15 18:31:16 +0530

    Created Profile Ui

--- app/src/main/java/com/example/medicman/Profile.java
+++ app/src/main/java/com/example/medicman/Profile.java
@@ -6,47 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        bottomNavigationView=findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.mprofile);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent i;
-                switch (item.getItemId()){
-                    case R.id.home:
-                        i =new Intent(getApplicationContext(),Home.class);
-                        startActivity(i);
-                        finish();
-                        overridePendingTransition(0,0);
-                        break;
-                    case R.id.info:
-                        i =new Intent(getApplicationContext(),Medicine_info.class);
-                        startActivity(i);
-                        finish();
-                        overridePendingTransition(0,0);
-                        break;
-                    case R.id.history:
-                        i =new Intent(getApplicationContext(),History.class);
-                        startActivity(i);
-                        finish();
-                        overridePendingTransition(0,0);
-                        break;
-                }
-                return false;
-            }
-        });
+    }
 
+    public void backfromprofile(View view) {
+        finish();
     }
 }
